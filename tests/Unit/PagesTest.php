2b740afc996a76115a003c50aec348115f0d8e9e
borg1980 <borg1980@gmail.com>
2025-02-11 09:12:23 +0100

    new setup of phpunit

---
+++ tests/Unit/PagesTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PagesTest extends TestCase
+{
+    public function test_audit_logs_view()
+    {
+        $user = \App\User::find(1);
+        $this->actingAs($user);
+        $response = $this->get(uri: '/admin/audit-logs');
+        $response->assertStatus(200);
+    }
+
+    public function test_tickets_view()
+    {
+        $user = \App\User::find(1);
+        $this->actingAs($user);
+        $response = $this->get(uri: '/admin/tickets');
+        $response->assertStatus(200);
+    }
+
+}
