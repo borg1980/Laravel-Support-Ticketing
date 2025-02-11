ff1697fc202f70d6e9be9890221667123047c705
borg1980 <borg1980@gmail.com>
2025-02-11 15:53:16 +0100

    do not create not needed folders in docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,25 +7,6 @@ php artisan cache:clear
 php artisan config:clear
 php artisan route:cache
 
-mkdir -p /var/www/html/storage/app/documents
-chown -R www-data:www-data /var/www/html/storage/app/documents
-
-mkdir -p /var/www/html/storage/app/elements
-mkdir -p /var/www/html/storage/app/elements/thumbs
-chown -R www-data:www-data /var/www/html/storage/app/elements
-
-mkdir -p /var/www/html/storage/app/empik
-mkdir -p /var/www/html/storage/app/empik/thumbs
-chown -R www-data:www-data /var/www/html/storage/app/empik
-
-mkdir -p /var/www/html/storage/app/images
-mkdir -p /var/www/html/storage/app/images/thumbs
-chown -R www-data:www-data /var/www/html/storage/app/images
-
-mkdir -p /var/www/html/storage/app/options
-mkdir -p /var/www/html/storage/app/options/thumbs
-chown -R www-data:www-data /var/www/html/storage/app/options
-
 chown -R www-data:www-data /var/www/html/storage/framework
 chown -R www-data:www-data /var/www/html/vendor
 
