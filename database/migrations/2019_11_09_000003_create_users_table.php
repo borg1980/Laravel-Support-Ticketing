7c0652097c658e8691d1e67fae5e5db97a37cf16
borg1980 <borg1980@gmail.com>
2025-02-11 18:18:41 +0100

    adding locale field to users table

--- database/migrations/2019_11_09_000003_create_users_table.php
+++ database/migrations/2019_11_09_000003_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->datetime('email_verified_at')->nullable();
+            $table->string('locale',2)->default('en');
             $table->string('password');
             $table->string('remember_token')->nullable();
             $table->timestamps();
