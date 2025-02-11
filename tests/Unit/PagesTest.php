d9916483b37ed354c20ed8c4b92532bed541123f
borg1980 <borg1980@gmail.com>
2025-02-11 09:28:49 +0100

    testing unit tests with gates

--- tests/Unit/PagesTest.php
+++ tests/Unit/PagesTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 
 class PagesTest extends TestCase
 {
+
     public function test_audit_logs_view()
     {
         $user = \App\User::find(1);
@@ -15,6 +16,7 @@ class PagesTest extends TestCase
         $response = $this->get(uri: '/admin/audit-logs');
         $response->assertStatus(200);
     }
+
 
     public function test_tickets_view()
     {
@@ -24,4 +26,14 @@ class PagesTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function test_comments_view()
+    {
+        $user = \App\User::find(1);
+        $this->actingAs($user);
+        \Gate::define('comment_access', function ($user) {
+            return true;
+        });
+        $response = $this->get(uri: '/admin/comments');
+        $response->assertStatus(200);
+    }
 }
