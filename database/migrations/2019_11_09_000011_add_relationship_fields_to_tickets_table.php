7abca9f4c65cc8d316d8d76be63817972876bac9
borg1980 <borg1980@gmail.com>
2025-02-11 16:52:52 +0100

    using anonymus classes for migrations - as in modern laravel

--- database/migrations/2019_11_09_000011_add_relationship_fields_to_tickets_table.php
+++ database/migrations/2019_11_09_000011_add_relationship_fields_to_tickets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRelationshipFieldsToTicketsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -19,4 +19,4 @@ class AddRelationshipFieldsToTicketsTable extends Migration
             $table->foreign('assigned_to_user_id', 'assigned_to_user_fk_583768')->references('id')->on('users');
         });
     }
-}
+};
--- database/migrations/2019_11_09_000012_add_relationship_fields_to_comments_table.php
+++ database/migrations/2019_11_09_000012_add_relationship_fields_to_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRelationshipFieldsToCommentsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -15,4 +15,4 @@ class AddRelationshipFieldsToCommentsTable extends Migration
             $table->foreign('user_id', 'user_fk_583777')->references('id')->on('users');
         });
     }
-}
+};
