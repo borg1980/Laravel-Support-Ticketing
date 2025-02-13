53f858157b4a16f38d213c1f9cb9c49f3655c798
borg1980 <borg1980@gmail.com>
2025-02-14 00:06:04 +0100

    fixing file upload on l11 native branch

--- app/Http/Controllers/Traits/MediaUploadingTrait.php
+++ app/Http/Controllers/Traits/MediaUploadingTrait.php
@@ -3,27 +3,28 @@
 namespace App\Http\Controllers\Traits;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 trait MediaUploadingTrait
 {
     public function storeMedia(Request $request)
     {
-// Validates file size
-        if (request()->has('size')) {
-            $this->validate(request(), [
-                'file' => 'max:' . request()->input('size') * 1024,
-            ]);
+        // Validates file size
+        if ($request->has('size')) {
+            Validator::make($request->all(), [
+                'file' => 'max:' . $request->input('size') * 1024,
+            ])->validate();
         }
 
-// If width or height is preset - we are validating it as an image
-        if (request()->has('width') || request()->has('height')) {
-            $this->validate(request(), [
+        // If width or height is preset - we are validating it as an image
+        if ($request->has('width') || $request->has('height')) {
+            Validator::make($request->all(), [
                 'file' => sprintf(
                     'image|dimensions:max_width=%s,max_height=%s',
-                    request()->input('width', 100000),
-                    request()->input('height', 100000)
+                    $request->input('width', 100000),
+                    $request->input('height', 100000)
                 ),
-            ]);
+            ])->validate();
         }
 
         $path = storage_path('tmp/uploads');
