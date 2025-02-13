93fd10210fbe9a305d378f9d9e6f4d862514a675
borg1980 <borg1980@gmail.com>
2025-02-13 18:45:21 +0100

    routing for API in Laravel11 style

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,38 @@
 <?php
 
-Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:api']], function (): void {
+use App\Http\Controllers\Api\V1\Admin\PermissionsApiController;
+use App\Http\Controllers\Api\V1\Admin\RolesApiController;
+use App\Http\Controllers\Api\V1\Admin\UsersApiController;
+use App\Http\Controllers\Api\V1\Admin\StatusesApiController;
+use App\Http\Controllers\Api\V1\Admin\PrioritiesApiController;
+use App\Http\Controllers\Api\V1\Admin\CategoriesApiController;
+use App\Http\Controllers\Api\V1\Admin\TicketsApiController;
+use App\Http\Controllers\Api\V1\Admin\CommentsApiController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'v1', 'as' => 'api.', 'middleware' => ['auth:api']], function (): void {
     // Permissions
-    Route::apiResource('permissions', 'PermissionsApiController');
+    Route::apiResource('permissions', PermissionsApiController::class);
 
     // Roles
-    Route::apiResource('roles', 'RolesApiController');
+    Route::apiResource('roles', RolesApiController::class);
 
     // Users
-    Route::apiResource('users', 'UsersApiController');
+    Route::apiResource('users', UsersApiController::class);
 
     // Statuses
-    Route::apiResource('statuses', 'StatusesApiController');
+    Route::apiResource('statuses', StatusesApiController::class);
 
     // Priorities
-    Route::apiResource('priorities', 'PrioritiesApiController');
+    Route::apiResource('priorities', PrioritiesApiController::class);
 
     // Categories
-    Route::apiResource('categories', 'CategoriesApiController');
+    Route::apiResource('categories', CategoriesApiController::class);
 
     // Tickets
-    Route::post('tickets/media', 'TicketsApiController@storeMedia')->name('tickets.storeMedia');
-    Route::apiResource('tickets', 'TicketsApiController');
+    Route::post('tickets/media', [TicketsApiController::class, 'storeMedia'])->name('tickets.storeMedia');
+    Route::apiResource('tickets', TicketsApiController::class);
 
     // Comments
-    Route::apiResource('comments', 'CommentsApiController');
+    Route::apiResource('comments', CommentsApiController::class);
 });
